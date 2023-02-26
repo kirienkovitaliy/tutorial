d4f899b421a0cf34fbc1275141f3ca8fe817516d
kirienkovitaliy <kirienkovitaliy1989@gmail.com>
2023-02-26 19:45:12 +0200

    Homework 11

--- oop_main.py
+++ oop_main.py
@@ -1,10 +1,20 @@
+import re
 from collections import UserDict
+from datetime import datetime
 
 
 class Field:
 
     def __init__(self, value):
-        self.value = value
+        self.__value = value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, new_value):
+        self.__value = new_value
 
 
 class Name(Field):
@@ -12,19 +22,55 @@ class Name(Field):
 
 
 class Phone(Field):
-    pass
+
+    def __init__(self, phone):
+        self.__phone = None
+        self.phone = phone
+
+    @property
+    def phone(self):
+        return self.__phone
+
+    @phone.setter
+    def phone(self, phone):
+        phone = re.sub(r'-|\(|\)|\+| ', '', phone)
+        phone = phone.strip()
+        self.__phone = phone
+
+
+class Birthday(Field):
+    def __init__(self, birthday):
+        self.__birthday = None
+        self.birthday = birthday
+
+    @property
+    def birthday(self):
+        return self.__birthday
+
+    @birthday.setter
+    def birthday(self, birthday):
+        if birthday == datetime.strptime(birthday, "%d.%m.%Y"):
+            self.__birthday = birthday
 
 
 class Record:
 
-    def __init__(self, name, phone: Phone = None, email=None):
+    def __init__(self, name, phone: Phone = None, email=None, birthday=None):
         self.name = name
         self.phones = []
         self.emails = []
+        self.birthday = birthday
         if phone:
             self.phones.append(phone)
         if email:
             self.emails.append(email)
+
+    def days_to_birthday(self):
+        if Birthday:
+            current_datetime = datetime.now()
+            user_db = datetime.strptime(Birthday, "%d.%m.%Y")
+            user_db = user_db.replace(year=current_datetime.year)
+            return (user_db - current_datetime).days
 
 
 class AddressBook(UserDict):
@@ -38,3 +84,8 @@ class AddressBook(UserDict):
     def change_record(self, name):
         if self.data.name:
             self.data.get(name)
+
+    def iterator(self, N):
+        for indx, val in enumerate(self.values, start=1):
+            if indx <= N:
+                yield val
